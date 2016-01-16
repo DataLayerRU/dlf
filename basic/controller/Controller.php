b518133909a5cb26d88d21067a0af35a00edfedf
SergioMadness <madness.sergio@gmail.com>
2016-01-16 22:51:01 +0300

    Current request in controller

--- basic/controller/Controller.php
+++ basic/controller/Controller.php
@@ -2,7 +2,24 @@
 
 namespace dlf\basic\controller;
 
+use dlf\web\Request;
+
 class Controller
 {
-    
+    private $request;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
 }
