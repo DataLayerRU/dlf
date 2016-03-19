63c3711623b83fdd52d0415195df4950b70c8687
SergioMadness <madness.sergio@gmail.com>
2016-03-20 02:46:11 +0300

    Tests

--- framework/basic/db/DBModel.php
+++ framework/basic/db/DBModel.php
@@ -2,7 +2,10 @@
 
 namespace pwf\basic\db;
 
-abstract class DBModel extends \pwf\components\activerecord\Model implements \pwf\components\querybuilder\interfaces\SelectBuilder, \pwf\components\querybuilder\interfaces\InsertBuilder, \pwf\components\querybuilder\interfaces\UpdateBuilder, \pwf\components\querybuilder\interfaces\DeleteBuilder {
+abstract class DBModel extends \pwf\components\activerecord\Model implements \pwf\components\querybuilder\interfaces\SelectBuilder,
+    \pwf\components\querybuilder\interfaces\InsertBuilder, \pwf\components\querybuilder\interfaces\UpdateBuilder,
+    \pwf\components\querybuilder\interfaces\DeleteBuilder
+{
 
     use \pwf\components\querybuilder\traits\Conditional,
         \pwf\components\querybuilder\traits\Parameterized,
@@ -16,7 +19,8 @@ abstract class DBModel extends \pwf\components\activerecord\Model implements \pw
      * @param \pwf\components\dbconnection\interfaces\Connection $connection
      * @param array $attributes
      */
-    public function __construct($connection, array $attributes = array()) {
+    public function __construct($connection, array $attributes = array())
+    {
         parent::__construct($connection, $attributes);
 
         $this->setConditionBuilder(QueryBuilder::getConditionBuilder());
@@ -26,71 +30,79 @@ abstract class DBModel extends \pwf\components\activerecord\Model implements \pw
      * 
      * @return type
      */
-    public function count() {
+    public function count()
+    {
         return (int) $this->getConnection()
-                        ->query(QueryBuilder::select()
-                                ->select(['COUNT(' . $this->getPK() . ') AS CNT'])
-                                ->table($this->getTable())
-                                ->setConditionBuilder($this->getConditionBuilder())
-                                ->where($this->getWhere())
-                                ->generate(), $this->getParams())
-                        ->fetchColumn('CNT');
+                ->query(QueryBuilder::select()
+                    ->select(['COUNT('.$this->getPK().') AS CNT'])
+                    ->table($this->getTable())
+                    ->setConditionBuilder($this->getConditionBuilder())
+                    ->where($this->getWhere())
+                    ->generate(), $this->getParams())
+                ->fetchColumn('CNT');
     }
 
     /**
      * @inheritdoc
      */
-    public function delete() {
+    public function delete()
+    {
         return $this->getConnection()->exec(QueryBuilder::delete()
-                                ->table($this->getTable())
-                                ->setConditionBuilder($this->getConditionBuilder())
-                                ->where([
-                                    $this->getPK() => $this->getId()
-                                ])
-                                ->generate(), $this->getParams());
-    }
-
-    public function getAll() {
-        return $this->getConnection()->query(QueryBuilder::select()
-                                ->table($this->getTable())
-                                ->setConditionBuilder($this->getConditionBuilder())
-                                ->where($condition)
-                                ->limit($limit)
-                                ->offset($offset)
-                                ->generate(), $this->getParams());
-    }
-
-    public function getOne() {
-        return $this->getConnection()->query(
-                        QueryBuilder::select()
-                                ->table($this->getTable())
-                                ->setConditionBuilder($this->getConditionBuilder())
-                                ->where($condition)
-                                ->limit(1)
-                                ->generate(), $this->getParams());
+                    ->table($this->getTable())
+                    ->setConditionBuilder($this->getConditionBuilder())
+                    ->where($this->getWhere())
+                    ->generate(), $this->getParams());
     }
 
     /**
      * @inheritdoc
      */
-    public function save() {
+    public function getAll()
+    {
+        return $this->getConnection()->query(QueryBuilder::select()
+                    ->table($this->getTable())
+                    ->setConditionBuilder($this->getConditionBuilder())
+                    ->where($this->getWhere())
+                    ->limit($this->getLimit())
+                    ->offset($this->getOffset())
+                    ->generate(), $this->getParams());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOne()
+    {
+        return $this->getConnection()->query(
+                QueryBuilder::select()
+                    ->table($this->getTable())
+                    ->setConditionBuilder($this->getConditionBuilder())
+                    ->where($this->getWhere())
+                    ->limit(1)
+                    ->generate(), $this->getParams());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function save()
+    {
         $result = null;
-        $id = $this->getId();
+        $id     = $this->getId();
         if (!empty($id)) {
             $result = $this->getConnection()->exec(QueryBuilder::update()
-                            ->table($this->getTable())
-                            ->setConditionBuilder($this->getConditionBuilder())
-                            ->where([
-                                $this->getPK() => $this->getId()
-                            ])
-                            ->setParams($this->getAttributes())
-                            ->generate(), $this->getParams());
+                    ->table($this->getTable())
+                    ->setConditionBuilder($this->getConditionBuilder())
+                    ->where([
+                        $this->getPK() => $this->getId()
+                    ])
+                    ->setParams($this->getAttributes())
+                    ->generate(), $this->getParams());
         } else {
             $result = $this->getConnection()->exec(QueryBuilder::insert()
-                            ->table($this->getTable())
-                            ->setConditionBuilder($this->getConditionBuilder())
-                            ->setParams($this->getAttributes())
-                            ->generate(), $this->getParams());
+                    ->table($this->getTable())
+                    ->setParams($this->getAttributes())
+                    ->generate(), $this->getParams());
         }
         return $result;
     }
@@ -99,15 +111,17 @@ abstract class DBModel extends \pwf\components\activerecord\Model implements \pw
      * @inheritdoc
      * @throws Exception
      */
-    public function generate() {
-        throw new Exception('Not implemented');
+    public function generate()
+    {
+        throw new \Exception('Not implemented');
     }
 
     /**
      * @inheritdoc
      */
-    public function getParams() {
-        return array_merge($this->parentGetParams(), $this->getConditionBuilder()->getParams());
+    public function getParams()
+    {
+        return array_merge($this->parentGetParams(),
+            $this->getConditionBuilder()->getParams());
     }
-
 }
--- tests/unit/DBModelTest.php
+++ tests/unit/DBModelTest.php
@@ -1,28 +1,22 @@
 <?php
 
-class DBModelTest extends \PHPUnit_Framework_TestCase {
-
+class DBModelTest extends \PHPUnit_Framework_TestCase
+{
     private static $model;
 
-    protected function setUp() {
+    protected function setUp()
+    {
         \pwf\basic\db\QueryBuilder::setDriver(\pwf\basic\db\QueryBuilder::DRIVER_MYSQL);
     }
 
-    protected function tearDown() {
-        self::$model = null;
-    }
-
-    public function testCount() {
+    public function testCount()
+    {
         $countQuery = 'SELECT COUNT(id) AS CNT FROM test_table WHERE field1=:field1 AND field2=:field2';
 
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection', [], [
-                    'query' => function($query, $params = []) use($countQuery) {
-
-                $this->assertEquals($query, $countQuery);
-
-                return new PDOStatement();
-            },
-                    'exec' => function($query, $params = [])use($countQuery) {
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'query' => function($query, $params = []) use($countQuery) {
 
                 $this->assertEquals($query, $countQuery);
 
@@ -30,21 +24,242 @@ class DBModelTest extends \PHPUnit_Framework_TestCase {
             }
         ]);
 
-        self::$model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel', [
-                    'connection' => $connection
-                        ], [
-                    'getId' => function() {
-                        return 1;
-                    }
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
         ]);
 
-        self::$model
-                ->table('test_table')
-                ->setPK('id')
-                ->where([
-                    'field1' => 1,
-                    'field2' => 2
-                ])->count([]);
+        $model
+            ->table('test_table')
+            ->setPK('id')
+            ->where([
+                'field1' => 1,
+                'field2' => 2
+            ])->count();
     }
 
+    public function testDelete()
+    {
+        $countQuery = 'DELETE FROM test_table WHERE field1=:field1 AND field2=:field2';
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'exec' => function($query, $params = [])use($countQuery) {
+
+                $this->assertEquals($query, $countQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setPK('id')
+            ->where([
+                'field1' => 1,
+                'field2' => 2
+            ])->delete();
+    }
+
+    public function testGetAll()
+    {
+        $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2';
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'query' => function($query, $params = []) use($countQuery) {
+
+                $this->assertEquals($query, $countQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setPK('id')
+            ->where([
+                'field1' => 1,
+                'field2' => 2
+            ])->getAll();
+    }
+
+    public function testGetOne()
+    {
+        $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2 LIMIT 1';
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'query' => function($query, $params = []) use($countQuery) {
+
+                $this->assertEquals($query, $countQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setPK('id')
+            ->where([
+                'field1' => 1,
+                'field2' => 2
+            ])->getOne();
+    }
+
+    public function testGenerate()
+    {
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => null
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        try {
+            $model->generate();
+            $this->assertTrue(false);
+        } catch (\Exception $ex) {
+
+        }
+    }
+
+    public function testSave()
+    {
+        $updateQuery = 'UPDATE test_table SET name=:name WHERE id=:id';
+
+        $insertQuery = 'INSERT INTO test_table (name) VALUES (:name)';
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'exec' => function($query, $params = [])use($updateQuery) {
+
+                $this->assertEquals($query, $updateQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setAttribute('name', 'asdasd')
+            ->setPK('id')
+            ->save();
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'exec' => function($query, $params = [])use($insertQuery) {
+
+                $this->assertEquals($query, $insertQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return null;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setAttribute('name', 'asdasd')
+            ->setPK('id')
+            ->save();
+    }
+
+    /**************Postgre***************/
+    public function testGetOnePG()
+    {
+        \pwf\basic\db\QueryBuilder::setDriver(\pwf\basic\db\QueryBuilder::DRIVER_PG);
+
+        $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2 LIMIT 1';
+
+        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                [],
+                [
+                'query' => function($query, $params = []) use($countQuery) {
+
+                $this->assertEquals($query, $countQuery);
+
+                return new PDOStatement();
+            }
+        ]);
+
+        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                [
+                'connection' => $connection
+                ],
+                [
+                'getId' => function() {
+                    return 1;
+                }
+        ]);
+
+        $model
+            ->table('test_table')
+            ->setPK('id')
+            ->where([
+                'field1' => 1,
+                'field2' => 2
+            ])->getOne();
+    }
 }
