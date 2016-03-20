06c5e22fc507aca3cc6009efbcf96bf7cf41b74d
SergioMadness <madness.sergio@gmail.com>
2016-03-20 20:34:08 +0300

    DBModel test

--- tests/unit/DBModelTest.php
+++ tests/unit/DBModelTest.php
@@ -1,148 +1,282 @@
 <?php
 
+use pwf\basic\db\QueryBuilder;
+
 class DBModelTest extends \PHPUnit_Framework_TestCase
 {
-    private static $model;
-
-    protected function setUp()
-    {
-        \pwf\basic\db\QueryBuilder::setDriver(\pwf\basic\db\QueryBuilder::DRIVER_MYSQL);
-    }
+    private $drivers = [
+        QueryBuilder::DRIVER_MYSQL,
+        QueryBuilder::DRIVER_PG,
+        -1
+    ];
 
     public function testCount()
     {
         $countQuery = 'SELECT COUNT(id) AS CNT FROM test_table WHERE field1=:field1 AND field2=:field2';
 
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'query' => function($query, $params = []) use($countQuery) {
+        foreach ($this->drivers as $driver) {
+            QueryBuilder::setDriver($driver);
 
-                $this->assertEquals($query, $countQuery);
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'query' => function($query, $params = []) use($countQuery) {
 
-                return new PDOStatement();
-            }
-        ]);
+                    $this->assertEquals($query, $countQuery);
 
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
+                    return new PDOStatement();
                 }
-        ]);
+            ]);
 
-        $model
-            ->table('test_table')
-            ->setPK('id')
-            ->where([
-                'field1' => 1,
-                'field2' => 2
-            ])->count();
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return 1;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setPK('id')
+                    ->where([
+                        'field1' => 1,
+                        'field2' => 2
+                    ])->count();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+        }
     }
 
     public function testDelete()
     {
         $countQuery = 'DELETE FROM test_table WHERE field1=:field1 AND field2=:field2';
 
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'exec' => function($query, $params = [])use($countQuery) {
+        foreach ($this->drivers as $driver) {
+            QueryBuilder::setDriver($driver);
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'exec' => function($query, $params = [])use($countQuery) {
 
-                $this->assertEquals($query, $countQuery);
+                    $this->assertEquals($query, $countQuery);
 
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
+                    return new PDOStatement();
                 }
-        ]);
+            ]);
 
-        $model
-            ->table('test_table')
-            ->setPK('id')
-            ->where([
-                'field1' => 1,
-                'field2' => 2
-            ])->delete();
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return 1;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setPK('id')
+                    ->where([
+                        'field1' => 1,
+                        'field2' => 2
+                    ])->delete();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+        }
     }
 
     public function testGetAll()
     {
         $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2';
 
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'query' => function($query, $params = []) use($countQuery) {
+        foreach ($this->drivers as $driver) {
+            QueryBuilder::setDriver($driver);
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'query' => function($query, $params = []) use($countQuery) {
 
-                $this->assertEquals($query, $countQuery);
+                    $this->assertEquals($query, $countQuery);
 
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
+                    return new PDOStatement();
                 }
-        ]);
+            ]);
 
-        $model
-            ->table('test_table')
-            ->setPK('id')
-            ->where([
-                'field1' => 1,
-                'field2' => 2
-            ])->getAll();
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return 1;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setPK('id')
+                    ->where([
+                        'field1' => 1,
+                        'field2' => 2
+                    ])->getAll();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+        }
     }
 
     public function testGetOne()
     {
         $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2 LIMIT 1';
 
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'query' => function($query, $params = []) use($countQuery) {
+        foreach ($this->drivers as $driver) {
+            QueryBuilder::setDriver($driver);
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'query' => function($query, $params = []) use($countQuery) {
 
-                $this->assertEquals($query, $countQuery);
+                    $this->assertEquals($query, $countQuery);
 
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
+                    return new PDOStatement();
                 }
-        ]);
+            ]);
 
-        $model
-            ->table('test_table')
-            ->setPK('id')
-            ->where([
-                'field1' => 1,
-                'field2' => 2
-            ])->getOne();
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return 1;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setPK('id')
+                    ->where([
+                        'field1' => 1,
+                        'field2' => 2
+                    ])->getOne();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+        }
+    }
+
+//    public function testGenerate()
+//    {
+//        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+//                [
+//                'connection' => null
+//                ],
+//                [
+//                'getId' => function() {
+//                    return 1;
+//                }
+//        ]);
+//
+//        try {
+//            $model->generate();
+//            $this->assertTrue(false);
+//        } catch (\Exception $ex) {
+//
+//        }
+//    }
+
+    public function testSave()
+    {
+        $updateQuery = 'UPDATE test_table SET name=:name WHERE id=:id';
+
+        $insertQuery = 'INSERT INTO test_table (name) VALUES (:name)';
+
+        foreach ($this->drivers as $driver) {
+            QueryBuilder::setDriver($driver);
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'exec' => function($query, $params = [])use($updateQuery) {
+
+                    $this->assertEquals($query, $updateQuery);
+
+                    return new PDOStatement();
+                }
+            ]);
+
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return 1;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setAttribute('name', 'asdasd')
+                    ->setPK('id')
+                    ->save();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+
+            $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
+                    [],
+                    [
+                    'exec' => function($query, $params = [])use($insertQuery) {
+
+                    $this->assertEquals($query, $insertQuery);
+
+                    return new PDOStatement();
+                }
+            ]);
+
+            $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
+                    [
+                    'connection' => $connection
+                    ],
+                    [
+                    'getId' => function() {
+                        return null;
+                    }
+            ]);
+
+            try {
+                $model
+                    ->table('test_table')
+                    ->setAttribute('name', 'asdasd')
+                    ->setPK('id')
+                    ->save();
+            } catch (Exception $ex) {
+                if ($driver != -1) {
+                    $this->assertTrue(false);
+                }
+            }
+        }
     }
 
     public function testGenerate()
@@ -163,103 +297,5 @@ class DBModelTest extends \PHPUnit_Framework_TestCase
         } catch (\Exception $ex) {
 
         }
-    }
-
-    public function testSave()
-    {
-        $updateQuery = 'UPDATE test_table SET name=:name WHERE id=:id';
-
-        $insertQuery = 'INSERT INTO test_table (name) VALUES (:name)';
-
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'exec' => function($query, $params = [])use($updateQuery) {
-
-                $this->assertEquals($query, $updateQuery);
-
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
-                }
-        ]);
-
-        $model
-            ->table('test_table')
-            ->setAttribute('name', 'asdasd')
-            ->setPK('id')
-            ->save();
-
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'exec' => function($query, $params = [])use($insertQuery) {
-
-                $this->assertEquals($query, $insertQuery);
-
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return null;
-                }
-        ]);
-
-        $model
-            ->table('test_table')
-            ->setAttribute('name', 'asdasd')
-            ->setPK('id')
-            ->save();
-    }
-
-    /**************Postgre***************/
-    public function testGetOnePG()
-    {
-        \pwf\basic\db\QueryBuilder::setDriver(\pwf\basic\db\QueryBuilder::DRIVER_PG);
-
-        $countQuery = 'SELECT * FROM test_table WHERE field1=:field1 AND field2=:field2 LIMIT 1';
-
-        $connection = Codeception\Util\Stub::construct('\pwf\components\dbconnection\PDOConnection',
-                [],
-                [
-                'query' => function($query, $params = []) use($countQuery) {
-
-                $this->assertEquals($query, $countQuery);
-
-                return new PDOStatement();
-            }
-        ]);
-
-        $model = Codeception\Util\Stub::construct('\pwf\basic\db\DBModel',
-                [
-                'connection' => $connection
-                ],
-                [
-                'getId' => function() {
-                    return 1;
-                }
-        ]);
-
-        $model
-            ->table('test_table')
-            ->setPK('id')
-            ->where([
-                'field1' => 1,
-                'field2' => 2
-            ])->getOne();
     }
 }
